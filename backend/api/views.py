7f075e8ddf00023dd13d4164bfb66184735970e8
epselont <newrol35@gmail.com>
2024-04-03 17:27:30 +0300

    Added api, serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from todo.models import Todo
 
-# Create your views here.
+from .serializers import TodoSerializer
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
